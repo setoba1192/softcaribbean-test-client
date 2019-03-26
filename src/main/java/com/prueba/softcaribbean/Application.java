fab7d44cd1824766e158226f5fcd17d062fc58ce
Joan <setoba1192@gmail.com>
2019-03-26 01:21:45 -0500

    Primer commit softcaribbean test client

--- src/main/java/com/prueba/softcaribbean/Application.java
+++ src/main/java/com/prueba/softcaribbean/Application.java
@@ -11,12 +11,6 @@ public class Application {
 
 	public static void main(String[] args) {
 
-		logger.debug("This is debug message");
-		logger.info("This is info message");
-		logger.warn("This is warn message");
-		logger.fatal("This is fatal message");
-		logger.error("This is error message");
-
 		new SpringApplicationBuilder(Application.class).headless(false).web(false).run(args);
 	}
 
